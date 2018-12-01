0f34c282caddbd1d2b4c122ca22cafbeb16e6793
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2018-12-01 10:52:22 +0330

    Correct the ActionBar's back button navigation

--- presentation/src/main/java/ir/fallahpoor/vicinity/presentation/MainActivity.kt
+++ presentation/src/main/java/ir/fallahpoor/vicinity/presentation/MainActivity.kt
@@ -3,18 +3,29 @@ package ir.fallahpoor.vicinity.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import ir.fallahpoor.vicinity.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+        val navController = findNavController(R.id.navHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return (navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp())
     }
 
 }
